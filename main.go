8252d8d290540e9e2ed9931d4e03979c1ab57168
Antoine Deschênes <antoine.deschenes@equisoft.com>
2020-02-11 07:55:27 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/pelletier/go-toml"
+)
+
+func main() {
+	flag.Usage = func () {
+		_, _ = fmt.Fprintln(os.Stderr, "Usage: toml CMD FILE [QUERY] [VALUE]")
+		_, _ = fmt.Fprintln(os.Stderr, "")
+		_, _ = fmt.Fprintln(os.Stderr, "Commands:")
+		_, _ = fmt.Fprintln(os.Stderr, "lint", "output linted file")
+		_, _ = fmt.Fprintln(os.Stderr, "get ", "get query result")
+		_, _ = fmt.Fprintln(os.Stderr, "set ", "set value in toml file and save")
+	}
+	flag.Parse()
+
+	cmd := flag.Arg(0)
+	filename := flag.Arg(1)
+	query := flag.Arg(2)
+	value := flag.Arg(3)
+
+	if flag.NArg() < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	data, err := toml.LoadFile(filename)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(-1)
+	}
+
+	if cmd == "get" && flag.NArg() <= 3 {
+		path := getPath(query)
+		value := data.GetPath(path)
+		if value != nil {
+				fmt.Print(value)
+		}
+	} else if cmd == "lint" && flag.NArg() == 2 {
+		err := writeTomlFile(filename, data)
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	} else if cmd == "set" && flag.NArg() == 4 {
+		path := getPath(query)
+		data.SetPath(path, value)
+		err := writeTomlFile(filename, data)
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	} else {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+}
+
+func getPath(query string) []string {
+	regex := regexp.MustCompile(`([^."']+)|"([^"]*)"|'([^']*)'`)
+	matches := regex.FindAllStringSubmatch(query, -1)
+
+	var path []string
+	for _, e := range matches {
+		path = append(path, strings.Join(e[1:], ""))
+	}
+
+	return path
+}
+
+func writeTomlFile(filename string, toml *toml.Tree) error {
+	return ioutil.WriteFile(filename, []byte(toml.String()), 0644)
+}
